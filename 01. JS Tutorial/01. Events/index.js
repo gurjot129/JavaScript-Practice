73f537942ed3dd41a0c06a0286f2ebbb72381164
Gurjot Saini <gurjot.saini129@gmail.com>
2024-12-15 21:10:50 -0500

    fixes the issue of accessing changeText function, the function needs to be defined outside of the addEventListener, in order for the function to be global and accessible

--- 01. JS Tutorial/01. Events/index.js
+++ 01. JS Tutorial/01. Events/index.js
@@ -1,9 +1,10 @@
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", () => {
-
     console.log("DOM fully loaded and parsed!");
-
-    function changeText() {
-        document.getElementById("Placeholder").innerHTML = "Button was successfully clicked!";
-    }
 });
+
+
+//changes text of paragraph
+function changeText() {
+    document.getElementById('Placeholder').innerHTML = "Button was successfully clicked!";
+}
